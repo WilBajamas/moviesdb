d8e7819400b8028357fc11c31522bb3b64b2024b
Wilbert Tan <wilbert@snappymob.com>
2023-04-08 15:25:25 +0800

    Initial commit

---
+++ app/src/main/java/alex/example/movies/MainActivity.kt
@@ -0,0 +1,12 @@
+package alex.example.movies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import alex.example.movies.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
