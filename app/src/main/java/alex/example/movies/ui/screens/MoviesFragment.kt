ad8c25c97f308026bc3e1fabf28d3d38e2c03bd7
Wilbert Tan <wilbert@snappymob.com>
2023-04-10 23:20:23 +0800

    Set swipe refresh layout

--- app/src/main/java/alex/example/movies/ui/screens/MoviesFragment.kt
+++ app/src/main/java/alex/example/movies/ui/screens/MoviesFragment.kt
@@ -3,15 +3,14 @@ package alex.example.movies.ui.screens
 import alex.example.movies.ui.viewmodels.MoviesFragmentViewModel
 import alex.example.movies.R
 import alex.example.movies.databinding.FragmentMoviesBinding
+import alex.example.movies.domain.Movies
+import alex.example.movies.domain.ShowFilter
 import alex.example.movies.ui.adapters.FilterAdapter
 import alex.example.movies.ui.adapters.MoviesAdapter
 import alex.example.movies.utils.BaseFragment
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 
@@ -26,15 +25,47 @@ class MoviesFragment : BaseFragment<FragmentMoviesBinding, MoviesFragmentViewMod
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // TODO: Remove and use real-world API
+         val filterData = listOf(
+            ShowFilter(getString(R.string.popularity_descending)),
+            ShowFilter(getString(R.string.popularity_ascending)),
+            ShowFilter(getString(R.string.rating_descending)),
+            ShowFilter(getString(R.string.rating_ascending)),
+            ShowFilter(getString(R.string.release_date_descending)),
+            ShowFilter(getString(R.string.release_date_ascending)),
+            ShowFilter(getString(R.string.title_a_z)),
+        )
+
+        val testMoviesData = listOf<Movies>(
+            Movies("SampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+            Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
+        )
+
         with(binding) {
             moviesRv.layoutManager = GridLayoutManager(context, 2)
             filtersRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
-            moviesAdapter = MoviesAdapter(viewModel.testMoviesData)
-            filterAdapter = FilterAdapter(viewModel.filterData)
+            moviesAdapter = MoviesAdapter(testMoviesData)
+            filterAdapter = FilterAdapter(filterData)
 
             moviesRv.adapter = moviesAdapter
             filtersRv.adapter = filterAdapter
+
+            swipeRefreshLayout.setOnRefreshListener {
+                Log.i("REFRESH MOVIES:", "Refreshed")
+                // TODO: Call this only after API call failed/succeeded
+                swipeRefreshLayout.isRefreshing = false
+            }
         }
     }
 
--- app/src/main/java/alex/example/movies/ui/viewmodels/MoviesFragmentViewModel.kt
+++ app/src/main/java/alex/example/movies/ui/viewmodels/MoviesFragmentViewModel.kt
@@ -6,32 +6,5 @@ import androidx.lifecycle.ViewModel
 
 class MoviesFragmentViewModel : ViewModel() {
 
-    // TODO: Use real world data
-    val testMoviesData = listOf<Movies>(
-        Movies("SampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSampleSample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-        Movies("Sample", "Mar 01, 2023", "https://image.tmdb.org/t/p/original/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg", "100%"),
-    )
-
-    val filterData = listOf<ShowFilter>(
-        ShowFilter("Popular"),
-        ShowFilter("Release Date"),
-        ShowFilter("A-Z"),
-        ShowFilter("A-Z"),
-        ShowFilter("A-Z"),
-        ShowFilter("A-Z"),
-        ShowFilter("A-Z"),
-        ShowFilter("A-Z"),
-        ShowFilter("A-Z"),
-    )
 
 }
