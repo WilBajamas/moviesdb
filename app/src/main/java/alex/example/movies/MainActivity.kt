43feb4de24a8e6655729f437be8403057ddc0282
Wilbert Tan <wilbert@snappymob.com>
2023-04-13 02:30:09 +0800

    Add splash screen

--- app/src/main/java/alex/example/movies/MainActivity.kt
+++ app/src/main/java/alex/example/movies/MainActivity.kt
@@ -3,6 +3,7 @@ package alex.example.movies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import alex.example.movies.databinding.ActivityMainBinding
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
