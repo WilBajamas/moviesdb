e8cd4f1c98d325e21b1ec167975ed090db98dfe6
Wilbert Tan <wilbert@snappymob.com>
2023-04-28 01:57:42 +0800

    Login - update text fields behaviour

--- app/src/main/java/alex/example/movies/ui/screens/login/LoginFragment.kt
+++ app/src/main/java/alex/example/movies/ui/screens/login/LoginFragment.kt
@@ -7,22 +7,55 @@ import alex.example.movies.databinding.FragmentLoginBinding
 
 import alex.example.movies.ui.viewmodels.login.LoginFragmentViewModel
 import alex.example.movies.utils.BaseFragment
+import android.util.Patterns
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginFragmentViewModel>(
-    FragmentLoginBinding::inflate,
-    LoginFragmentViewModel::class.java
+    FragmentLoginBinding::inflate, LoginFragmentViewModel::class.java
 ) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
-            loginBtn.setOnClickListener {
-                findNavController().navigate(R.id.action_loginFragment_to_newOnboardingFragment)
-            }
-        }
 
+            emailInput.addTextChangedListener {
+                if (checkEmailInputValid()) disableFieldError(emailAddressTextField)
+            }
+
+            passwordInput.addTextChangedListener {
+                if (checkPasswordInputValid()) disableFieldError(passwordTextField)
+            }
+
+            loginBtn.setOnClickListener {
+
+                if (checkPasswordInputValid() && checkEmailInputValid()) {
+                    findNavController().navigate(R.id.action_loginFragment_to_newOnboardingFragment)
+                }
+
+                if (!checkEmailInputValid()) emailAddressTextField.error =
+                    getString(R.string.email_format_error)
+
+                if (!checkPasswordInputValid()) passwordTextField.error =
+                    getString(R.string.password_too_short)
+            }
+
+        }
+    }
+
+    private fun checkEmailInputValid(): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(binding.emailInput.text.toString().trim()).matches()
+    }
+
+    private fun checkPasswordInputValid(): Boolean {
+        return binding.passwordInput.text.toString().length > 7
+    }
+
+    private fun disableFieldError(inputLayout: TextInputLayout) {
+        inputLayout.isErrorEnabled = false
+        inputLayout.error = null
     }
 
 }
