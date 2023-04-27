afecbf6cc464a5426cdfce0a82da17666ab93c16
Wilbert Tan <wilbert@snappymob.com>
2023-04-28 03:11:16 +0800

    Login - add animation transition on button

--- app/src/main/java/alex/example/movies/ui/screens/login/LoginFragment.kt
+++ app/src/main/java/alex/example/movies/ui/screens/login/LoginFragment.kt
@@ -7,55 +7,106 @@ import alex.example.movies.databinding.FragmentLoginBinding
 
 import alex.example.movies.ui.viewmodels.login.LoginFragmentViewModel
 import alex.example.movies.utils.BaseFragment
-import android.util.Patterns
-import androidx.core.widget.addTextChangedListener
-import androidx.navigation.fragment.findNavController
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import androidx.core.widget.doOnTextChanged
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginFragment : BaseFragment<FragmentLoginBinding, LoginFragmentViewModel>(
     FragmentLoginBinding::inflate, LoginFragmentViewModel::class.java
 ) {
 
+    private var shortAnimationDuration: Int = 0
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
 
-            emailInput.addTextChangedListener {
-                if (checkEmailInputValid()) disableFieldError(emailAddressTextField)
+            emailInput.doOnTextChanged { text, _, _, _ ->
+                viewModel.email = text.toString().trim()
+                if (viewModel.checkEmailInputValid()) disableFieldError(emailAddressTextField)
             }
 
-            passwordInput.addTextChangedListener {
-                if (checkPasswordInputValid()) disableFieldError(passwordTextField)
+            passwordInput.doOnTextChanged { text, _, _, _ ->
+                viewModel.password = text.toString()
+                if (viewModel.checkPasswordInputValid()) disableFieldError(passwordTextField)
             }
 
             loginBtn.setOnClickListener {
 
-                if (checkPasswordInputValid() && checkEmailInputValid()) {
-                    findNavController().navigate(R.id.action_loginFragment_to_newOnboardingFragment)
+                if (viewModel.checkPasswordInputValid() && viewModel.checkEmailInputValid()) {
+//                    viewModel.login()
+//                    findNavController().navigate(R.id.action_loginFragment_to_newOnboardingFragment)
+                    fadeInLoading()
                 }
 
-                if (!checkEmailInputValid()) emailAddressTextField.error =
+                if (!viewModel.checkEmailInputValid()) emailAddressTextField.error =
                     getString(R.string.email_format_error)
 
-                if (!checkPasswordInputValid()) passwordTextField.error =
+                if (!viewModel.checkPasswordInputValid()) passwordTextField.error =
                     getString(R.string.password_too_short)
             }
 
         }
     }
 
-    private fun checkEmailInputValid(): Boolean {
-        return Patterns.EMAIL_ADDRESS.matcher(binding.emailInput.text.toString().trim()).matches()
-    }
-
-    private fun checkPasswordInputValid(): Boolean {
-        return binding.passwordInput.text.toString().length > 7
-    }
-
     private fun disableFieldError(inputLayout: TextInputLayout) {
         inputLayout.isErrorEnabled = false
         inputLayout.error = null
     }
+
+    private fun disableTextFields() {
+        binding.passwordTextField.isEnabled = false
+        binding.emailAddressTextField.isEnabled = false
+    }
+
+    private fun enableTextFields() {
+        binding.passwordTextField.isEnabled = false
+        binding.emailAddressTextField.isEnabled = false
+    }
+
+    private fun fadeInLoading() {
+        disableTextFields()
+        binding.progressIndicator.apply {
+            alpha = 0f
+            visibility = View.VISIBLE
+
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(null)
+        }
+        binding.loginBtn.animate()
+            .alpha(0f)
+            .setDuration(shortAnimationDuration.toLong())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.loginBtn.visibility = View.GONE
+                }
+            })
+    }
+
+    private fun fadeInButton() {
+        enableTextFields()
+        binding.loginBtn.apply {
+            alpha = 0f
+            visibility = View.VISIBLE
+
+            animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(null)
+        }
+        binding.progressIndicator.animate()
+            .alpha(0f)
+            .setDuration(shortAnimationDuration.toLong())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    binding.progressIndicator.visibility = View.GONE
+                }
+            })
+    }
+
 
 }
--- app/src/main/java/alex/example/movies/ui/viewmodels/login/LoginFragmentViewModel.kt
+++ app/src/main/java/alex/example/movies/ui/viewmodels/login/LoginFragmentViewModel.kt
@@ -1,6 +1,23 @@
 package alex.example.movies.ui.viewmodels.login
 
+import android.util.Patterns
 import androidx.lifecycle.ViewModel
 
-class LoginFragmentViewModel: ViewModel() {
+class LoginFragmentViewModel : ViewModel() {
+
+    var email: String? = null
+    var password: String? = null
+
+    fun login() {
+
+    }
+
+    fun checkEmailInputValid(): Boolean {
+        return !email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email.toString()).matches()
+    }
+
+    fun checkPasswordInputValid(): Boolean {
+        return !password.isNullOrEmpty() && password!!.length > 7
+    }
+
 }
